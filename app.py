bc9d7f6b18128714f2997e88430e3230a14dfbde
Anas Hussaindeen <anashussaindeen@gmail.com>
2024-10-11 09:32:57 +0530

    Updated dtaabase

--- app.py
+++ app.py
@@ -35,7 +35,9 @@ def get_results():
     vote_results = {team: count for team, count in votes}
     return jsonify(vote_results), 200
 
+# Ensure the database tables are created
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()  # Ensure the database tables are created
     app.run(debug=True)
